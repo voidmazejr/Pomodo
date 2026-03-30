d37be1531926f1127327601247eac97d7b565081
Luis Miguel Martins Mineiro <l.martins.mineiro@campus.tu-berlin.de>
2026-03-30 19:31:29 +0200

    Initial Commit

---
+++ Pomodu/PomoduApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomoduApp.swift
+//  Pomodu
+//
+//  Created by Luis Mineiro Jr on 30.03.26.
+//
+
+import SwiftUI
+
+@main
+struct PomoduApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
